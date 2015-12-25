7340bd7a107d2c0903bbf6b3a1757abbe8a2bae1
supermariolegend <email.marian@gmail.com>
2015-12-25 00:36:00 +0000

    Updated index.php
    Added new routes for error, search, categories, product, login routes,
    and admin routes

--- public/index.php
+++ public/index.php
@@ -1,9 +1,4 @@
 <?php
-// We want to ensure we have session
-if(session_id() === ""){
-    session_start();
-}
-
 // autoloader and other functions to include
 require_once __DIR__ .'/../vendor/autoload.php';
 require_once __DIR__ . '/../src/utility/helperFunctions.php';
@@ -18,16 +13,33 @@ $twig = new Twig_Environment($loader);
 //setup Silex
 $app = new Silex\Application();
 
+//register session provider with Silex
+$app->register(new Silex\Provider\SessionServiceProvider());
+
 //register Twig with Silex
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => $myTemplatesPath
 ));
 
-//map routes to controller class/method
+//********** map routes to controller class/method *************
 $app->get('/',      controller('KBK\Controller', 'main/index'));
 $app->get('/about', controller('KBK\Controller', 'main/about'));
 $app->get('/menu', controller('KBK\Controller', 'main/menu'));
 $app->get('/contact', controller('KBK\Controller', 'main/contact'));
+$app->get('/error', controller('KBK\Controller', 'main/error'));
+$app->get('/search', controller('KBK\Controller', 'main/search'));
+$app->get('/categories', controller('KBK\Controller', 'main/categories'));
+$app->get('/product', controller('KBK\Controller', 'main/product'));
 
+// ******** login routes GET **************
+$app->get('/login', controller('KBK\Controller', 'user/login'));
+$app->get('/logout', controller('KBK\Controller', 'user/logout'));
+
+// ********* login routes POST (process submitted form) ******
+$app->post('/login',    controller('KBK\Controller', 'user/processLogin'));
+
+// ******** secure pages ********************
+$app->get('/admin',     controller('KBK\Controller', 'admin/index'));
+$app->get('/adminCodes', controller('KBK\Controller', 'admin/codes'));
 //go - process request and decide what needed to be done
 $app->run();
