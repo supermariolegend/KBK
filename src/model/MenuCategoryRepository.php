8030a20cbc6c0910a30e20ad1e4d9a8f6f2656ed
supermariolegend <email.marian@gmail.com>
2016-01-04 22:33:17 +0000

    Updated MenuCategoryRepository
    Added a new function, getAllMenuCategories, and commented the file for
    phpDocumentor

--- src/model/MenuCategoryRepository.php
+++ src/model/MenuCategoryRepository.php
@@ -1,6 +1,10 @@
 <?php
-// see documentation folder for generated API doc for this class
 
+/**
+ * Class for storing all the menu categories in a single
+ * object. It provides a function which can easily extract
+ * all categories from the database
+ */
 
 namespace KBK\Model;
 
@@ -15,45 +19,29 @@ class MenuCategoryRepository
     /**
      * an associative array containing menu categories items objects which
      * are indexed by the unique PK field 'menuCategoryCode'
+     *
      * @var array
      */
     private $categories;
 
 
     /**
-     * create a new MenuCategoryRepository object and
-     * initialize it with a number of menu category products
+     * constructor for any new object
      */
+
     function __construct()
     {
-        $mc1 = new MenuCategory(1);
-        $mc1->setMenuCategoryTitle('Vegetarian Burgers');
-        $mc1->setMenuCategoryImage('vegetarian_burger.png');
-        $this->addMenuCategory($mc1);
-
-        $mc2 = new MenuCategory(2);
-        $mc2->setMenuCategoryTitle('Salads');
-        $mc2->setMenuCategoryImage('salad.jpg');
-        $this->addMenuCategory($mc2);
-
-        $mc3 = new MenuCategory(3);
-        $mc3->setMenuCategoryTitle('Drinks');
-        $mc3->setMenuCategoryImage('black_tea.png');
-        $this->addMenuCategory($mc3);
-
-        $mc4 = new MenuCategory(4);
-        $mc4->setMenuCategoryTitle('Desserts');
-        $mc4->setMenuCategoryImage('dessert.jpg');
-        $this->addMenuCategory($mc4);
-
+        $this->getAllMenuCategories();
     }
 
     /**
      * add the given menu item category to the repository
-     * please note that this is a private method to be used only by the constructor
+     * please note that this is a public method to be used
+     * by the constructor and in testing
+     *
      * @param MenuCategory $category
      */
-    private function addMenuCategory(MenuCategory $category)
+    public function addMenuCategory(MenuCategory $category)
     {
         // obtain ID from the menu category object
         $id = $category->getMenuCategoryCode();
@@ -63,17 +51,35 @@ class MenuCategoryRepository
     }
 
     /**
-     * return an array containing all menu categories
+     * return an array containing all menu categories after connecting to the database
+     *
      * @return array
      */
     public function getAllMenuCategories()
     {
+        $connection = open_database_connection();
+        $query = "SELECT * FROM `categories`";
+        $results = mysqli_query($connection, $query);
+        $this->categories = array();
+
+        if (mysqli_num_rows($results) > 0) {
+            while ($rows = $results->fetch_assoc()) {
+                $this->categories[$rows['CategoryID']] = new MenuCategory($rows['CategoryID']);
+                $this->categories[$rows['CategoryID']]->setMenuCategoryTitle($rows['CategoryName']);
+                $this->categories[$rows['CategoryID']]->setMenuCategoryImage($rows['CategoryImage']);
+                $this->categories[$rows['CategoryID']]->setMenuCategorySummary($rows['CategorySummary']);
+                $this->addMenuCategory($this->categories[$rows['CategoryID']]);
+            }
+        }
+
+        close_database_connection($connection);
         return $this->categories;
     }
 
     /**
      * this is a method that will try to retrieve one menu category item
      * for a given id = $id
+     *
      * @param int $id
      * @return menu category item (if one found)
      * @return null (if not found)
